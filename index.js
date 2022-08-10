bc945a367891e1b9fbbc84bea3a38cabc4740f20
Pandaphone <shawngreene075@gmail.com>
2022-08-10 19:39:27 -0400

    Done!

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(url, mLeft, mBottom){
+    let item = document.createElement('img')
+item.src = url
+item.style.position = 'fixed'
+item.style.left = mLeft + 'px'
+item.style.bottom = mBottom + 'px'
+document.body.append(item)
+}
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', 450, 200)
+
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+function newItem(url, mLeft, mBottom){
+    let object = document.createElement('img')
+object.src = url
+object.style.position = 'fixed'
+object.style.left = mLeft + 'px'
+object.style.bottom = mBottom + 'px'
+document.body.append(object)
+
+object.addEventListener('dblclick', function(){
+    object.remove()
+})
+}
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/shield.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
+
+
